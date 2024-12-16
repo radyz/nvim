d1d8b9bf696bc696d92f0b85626bde378ae45b13
Ernesto Chavez <ernesto.chavez@taxfyle.com>
2024-12-16 08:15:38 -0600

    chore: start adding some luadocs

--- lua/utils/text.lua
+++ lua/utils/text.lua
@@ -1,11 +1,20 @@
+--- Text utility functions
+-- @module text
 local M = {}
 
-M.trim = function(s)
-    return (s:gsub("^%s*(.-)%s*$", "%1"))
+--- It removes leading and trailing white space from the input string.
+--- @param input string
+--- @return string output string without leading and trailing white space.
+M.trim = function(input)
+    return (input:gsub("^%s*(.-)%s*$", "%1"))
 end
 
-M.startswith = function(text, prefix)
-    return text:find(prefix, 1, true) == 1
+--- Asserts whether the input string starts with prefix.
+--- @param input string
+--- @param prefix string
+--- @return boolean
+M.startswith = function(input, prefix)
+    return input:find(prefix, 1, true) == 1
 end
 
 return M
