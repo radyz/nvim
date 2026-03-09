177a25cdfefb04733067acc84a08977fd69469c9
Ernesto Chavez <ernesto.chavez@taxfyle.com>
2026-03-09 08:08:47 -0600

    replace old rest client with kulala

--- lua/plugins/http.lua
+++ lua/plugins/http.lua
@@ -1,15 +1,17 @@
 return {
     {
-        "rest-nvim/rest.nvim",
-        tag = "v1.1.0",
-        config = function()
-            require("rest-nvim").setup({
-                result_split_horizontal = true,
-                skip_ssl_verification = true,
-                result = {
-                    show_curl_command = false,
-                },
-            })
-        end,
+        "mistweaverco/kulala.nvim",
+        ft = { "http" },
+        opts = {},
+        keys = {
+            {
+                "<localleader>r",
+                function()
+                    require("kulala").run()
+                end,
+                mode = { "n", "v" },
+                desc = "Send request",
+            },
+        },
     },
 }
