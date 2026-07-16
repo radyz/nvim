b1f75d5b4684efe112202fcc027e99f98e81479f
Ernesto Chavez <ernesto.chavez@taxfyle.com>
2026-07-16 10:54:08 -0600

    fix: setup treesitter correctly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
         branch = "main",
         lazy = false,
         build = ":TSUpdate",
-        init = function()
+        config = function()
             require("nvim-treesitter").install({
                 "rust",
                 "typescript",
@@ -24,15 +24,11 @@ return {
                 "sql",
             })
 
-            local ts = require("vim.treesitter")
-            ts.query.add_directive("set_if_eq!", function(match, pattern, bufnr, predicate, metadata)
-                local _, key, capture_id, rhs = unpack(predicate)
-
-                local node = match[capture_id]
-                if node and ts.get_node_text(node, bufnr) == rhs then
-                    metadata[key] = true
-                end
-            end)
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
+            })
         end,
     },
 }
